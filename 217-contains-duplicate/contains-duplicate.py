843810f16d09490c517ef2295374380e52221132
Lakshay Kamat <lakshaykamat.dev@gmail.com>
2024-08-16 11:46:58 +0530

    Time: 483 ms (9.74%) | Memory: 21 MB (99.84%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        nums.sort()
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i - 1]:
+                return True
+        
+        return False
+
+
+        
